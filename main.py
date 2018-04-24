77a55badc81ae8f42c32dab2aed46b8b32016fec
fanfreak2247 <keandrewinston@gmail.com>
2018-04-24 18:50:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,69 @@
-import lines
+import geometry
 
-still_select = True
-
-
-print("Welcome to the Ultimate Math Tool. What would you like to do?")
+print("Welcome to the Ultimate Math Tool. What would you like to do?\n")
 while True:
-  print("Select from the following:\n1 - Area \n2 - Perimeter\n3 - Lines")
-  choice = input("")
-  if choice == "1":
-    while still_select:
-      choice = input("What shape would you like to calculate the area of?\n")
-      if choice.lower() == "square":
-        side = float(input("Please enter the side length of the square.\n"))
-        print(f'Area of the Square is: {lines.areaOfSquare(side)}\n')
-      if choice.lower() == "circle":
-        radius = float(input("Please enter the radius of the circle."))
-        print (f'The area of the circle is: {lines.areaOfCircle(radius)}\n')
-      if choice.lower() == "triangle":
-        base = float(input("Please enter the base of the triangle."))
-        height = float(input("Please enter the height of the triangle."))
-        print (f'The area of the triangle is: {lines.areaOfTriangle(base,height)}')
-      if choice.lower() == "rectangle":
-        length = float(input("Please input the length of the rectangle."))
-        width = float(input("Please enter the width of the rectangle."))
-        print (f'The area of the rectangle is: {lines.areaOfRectangle(length,width)}')
-      else: 
-       print("That is not a valid shape please try again.")
-        
-      
-      choice = input("Would you like to calculate the area of another shape?")
-      
-    
-  elif choice == "2":
-    
-  else:
-    print("That is not a valid option. Please try again.\n")
-  
+    choice = input("1 - Area\n2 - Perimeter\n3 - Lines and Stuff\n")
+    if choice == "1":
+        while True:
+            choice = input("What would you like to calculate the area of?\n 1 - Regular Polygons\n 2 - Triangle\n 3 - Rectangle\n 4 - Trapezoid\n")
+            #Reg Polygons
+            if choice == "1":
+                numOfSides = float(input("Enter in the amount of sides."))
+                sideLength = float(input("Enter in the side length."))
+                print(f'The area is: {geometry.areaRegularPolygons(numOfSides,sideLength)}\n')
+            #Triangle
+            if choice == "2":
+                base = float(input("Enter in the base length of the triangle."))
+                height = float(input("Enter in the height of the triangle."))
+                print(f'The area of the triangle is: {geometry.areaOfTriangle(base,height)}\n')
+            #Rectangle
+            if choice == "3":
+                length = float(input("Please enter the length of the rectangle."))
+                width = float(input("Please enter the width."))
+                print(f'The area of the rectangle is: {geometry.areaOfRectangle(length,width)}\n')
+            #Trapezoid
+            if choice == "4":
+                a = float(input("Please enter the a of the trapezoid."))
+                b = float(input("Please enter the b of the trapezoid."))
+                h = float(input("Please enter the h of the trapezoid."))
+                print(f'The area of the trapezoid is: {geometry.areaOfTrapezoid(a,b,h)}\n')
+            else:
+                print("That is not a valid option.")
+            choice = input("Would you still like to calculate area?\n")
+            if choice.lower() != "yes":
+                break
+    if choice == "2":
+        while True:
+            choice = input("What would you like to calculate the perimeter of?\n1 - Regular Polygon\n2 - Triangle\n3 - Rectangle\n4 - Trapezoid")
+            if choice == "1":
+                numOfSides = float(input("Enter in the amount of sides of the regular polygon."))
+                sideLength = float(input("Enter in the length of each side."))
+                print(f'The perimeter of the regular polygon is: {geometry.perimeterRegularPolygons(numOfSides,sideLength)}\n')
+            if choice == "2":
+                a = float(input("Enter in the a of the triangle."))
+                b = float(input("Enter in the b of the triangle."))
+                c = float(input("Enter in the c of the triangle."))
+                print(f'The perimeter of the triangle is: {geometry.perimeterOfTriangle(a,b,c)}\n')
+            if choice == "3":
+                length = float(input("Enter the length of the rectangle."))
+                width = float(input("Enter in the width of the rectangle."))
+                print(f'The perimeter of the rectangle is: {geometry.perimeterOfRectangle(length,width)}\n')
+            if choice == "4":
+                a = float(input("Enter the a of the trapezoid."))
+                b = float(input("Enter the b of the trapezoid."))
+                c = float(input("Enter in the c of the trapezoid."))
+                d = float(input("Enter in the d of the trapezoid."))
+                print(f'The perimeter of the trapezoid is: {geometry.perimeterOfTrapezoid(a,b,c,d)}\n')
+            else:
+                print("That's not a valid option.")
+            choice = input("Would you still like to calculate perimeter?")
+            if choice.lower() != "yes":
+                break
+
+
+
+
+
+
+
+
