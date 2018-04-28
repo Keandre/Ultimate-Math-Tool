b8fe32c595b72ad974d7cebcdcfdef19a4e07d99
fanfreak2247 <keandrewinston@gmail.com>
2018-04-28 03:16:01 -0400

    Update Line.py
    - Used @property instead of storing so many values.
    - Added selfs where necessary.

--- Line.py
+++ Line.py
@@ -9,42 +9,31 @@ class Point:
         self.y =  y
         self.x =  x
 class Line:
-
     point1 = (0,0)
     point2 = (0,0)
-    midpoint = (0,0)
-    slope = Fraction(0)
-    angle_radians = 0
-    angle_degrees = 0
-    length = 0
-    equation = ""
-    rightBisector = ""
-    angle_radians = 0
-    angle_degrees = 0
 
     def __init__(self,point1 = (0,0),point2=(0,0)):
         self.point1 = point1
         self.point2 = point2
-        self.midpoint = midpoint()
-        self.slope = slope()
-        self.length = length()
-        self.equation = equation()
-        self.rightBisector = rightBisector()
-        angle_radians = 0
-        angle_degrees = 0
 
+    @property
+    def midpoint(self):
+        return ((self.point1[0]+self.point2[0])/2,(self.point1[1]+self.point2[1])/2)
 
-    def midpoint():
-        return ((point1[0]+point2[0])/2,(point1[1]+point2[1])/2)
+    @property
+    def slope(self):
+        try:
+            return Fraction((self.point2[1]-self.point1[1]) / (self.point2[0]-self.point1[0]))
+        except ZeroDivisionError:
+            return 0
 
-    def slope():
-        return Fraction((point2[1]-point1[1]) / (point2[0]-point1[0]))
+    @property
+    def length(self):
+        return ((self.point2[0]-self.point1[0])**2+(self.point2[1]-self.point1[1])**2)
 
-    def length():
-        return ((point2[0]-point1[0])**2+(point2[1]-point1[1])**2)
-
-    def equation():
-        b = point1[1] - slope() * point1[0]
+    @property
+    def equation(self):
+        b = self.point1[1] - self.slope() * self.point1[0]
 
         # Convert to Fraction if it's not a whole number
         b = Fraction(b) if type(b) != int else b
@@ -52,15 +41,16 @@ class Line:
         # Change to '+Fraction' if it's a negative number and '-Fraction' if it's a positive one.
         b = "- " + str(b)[1:] if str(b)[0] == "-" else "+ " + str(b)
 
-        return (f"y = {slope()} x {b}")
+        return (f"y = {self.slope()} x {b}")
 
-    def rightBisector():
+    @property
+    def rightBisector(self):
             # Make slope and make it negative reciporcal
-            nume = slope().numerator
-            deno = slope().denominator
+            nume = self.slope().numerator
+            deno = self.slope().denominator
             negreci_slope = Fraction(-deno, nume)
 
-            b = midpoint()[1] - negreci_slope * midpoint()[0]
+            b = self.midpoint()[1] - negreci_slope * self.midpoint()[0]
 
             # Convert to Fraction if it's not a whole number
             b = Fraction(b) if int(b) != b else int(b)
@@ -73,20 +63,18 @@ class Line:
 
             return (f'y =  {negreci_slope}x {b}')
 
-    def angle_radians():
-        return 2*tan(point2[1]-point1[1],point2[0],point1[0])
+    @property
+    def angle_radians(self):
+        return 2*tan(self.point2[1]-self.point1[1],self.point2[0],self.point1[0])
 
-    def angle_degrees():
-        return 2*tan(point2[1]-point1[1],point2[0],point1[0])*180/pi
+    @property
+    def angle_degrees(self):
+        return 2*tan(self.point2[1]-self.point1[1],self.point2[0],self.point1[0])*180/pi
 
-    def onLine(point = (0,0)):
-        b = point1[1] - slope() * point1[0]
+    def onLine(self, point=(0, 0)):
+        b = self.point1[1] - self.slope() * self.point1[0]
 
         # Convert to Fraction if it's not a whole number
         b = Fraction(b) if type(b) != int else b
 
-        return (point[0]*slope() + b == point[1])
-
-
-
-
+        return (self.point[0] * self.slope() + b == self.point[1])
