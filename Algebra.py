22574be03dd67973aa7928dc12c26b216840974e
fanfreak2247 <keandrewinston@gmail.com>
2018-05-02 01:14:45 -0400

    Add files via upload

---
+++ Algebra.py
@@ -0,0 +1,11 @@
+"""
+The Ultimate Math Tool by FanFreak247 & DiracDelta
+Written in Python 3.6.5
+Copyright © FanFreak247 2018.
+This code is not to be used, redistributed or copied without the author's permission.
+"""
+from numpy import linalg
+from numpy import array
+
+
+#Currently in development.
